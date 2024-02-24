a3b56d7a12467548fb6cff25948ff0aede7b42a6
Amstrong1 <lewisamstrongh@gmail.com>
2024-02-24 14:56:13 +0100

    footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,38 @@
+<!-- TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com -->
+<!--Footer container-->
+<footer class="bg-secondary-100 text-center">
+    <!--Sign-up form section-->
+    <div class="px-6 pt-6">
+        <form action="">
+            <div class="gird-cols-1 grid items-center justify-center gap-4 md:grid-cols-3">
+                <div class="md:mb-6 md:ml-auto">
+                    <p class="text-secondary-800">
+                        <strong>Abonnez vous à notre newsletter</strong>
+                    </p>
+                </div>
+
+                <div class="relative md:mb-6" data-te-input-wrapper-init>
+                    <input type="text"
+                        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                        id="exampleFormControlInput1" placeholder="Email" />
+                    <label for="exampleFormControlInput1"
+                        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-secondary-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none">Email
+                    </label>
+                </div>
+
+                <div class="mb-6 md:mr-auto">
+                    <button type="button"
+                        class="inline-block rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]" style="background: rgb(247, 148, 29);" data-te-ripple-init data-te-ripple-color="light">
+                        Envoyer
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!--Copyright section-->
+    <div class="bg-black p-4 text-center text-white">
+        © 2023 Copyright:
+        <a class="text-white" href="https://tw-elements.com/">Open Send</a>
+    </div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,6 +88,8 @@
         </div>
     </main>
 
+    @include('footer')
+
     <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/tw-elements.umd.min.js"></script>
 </body>
 
